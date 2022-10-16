d615fcad111c6fc0394076ad6d14f6c4d58a867b
roy-jyotirmoy <roy.jyotirmoy095@gmail.com>
2022-10-16 12:26:17 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fce77f42-2f0e-4cc8-afbd-baedb3799a7e';
+export const MODAL_CLOSE_SEC = 2.5;
